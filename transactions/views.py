f46de8f59afa4a69b3dbea35b90a9cb772f930a7
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2026-01-07 10:36:25 +0530

    Sdsd

--- transactions/views.py
+++ transactions/views.py
@@ -79,7 +79,8 @@ def add_transaction(request):
         consignor_builty_count = builty.objects.filter(
             consignor=consignor_instance, 
             DC_date__gte=start_date, 
-            DC_date__lte=end_date
+            DC_date__lte=end_date,
+            deleted=False
         ).count()
 
         builty_code = builty_code + '-' + str(consignor_builty_count + 1)
